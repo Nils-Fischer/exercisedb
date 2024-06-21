f75a4d2e77c59d6f43c9de3fe7b7d70444bf7c1a
Eren <erendemir10022@gmail.com>
2024-06-21 14:10:58 +0200

    priority and algorithm

---
+++ src/components/WorkoutAlgo.svelte
@@ -0,0 +1,16 @@
+Nigga nigga niggaNigga nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga
+niggaNigga nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga
+nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga nigga
+niggaNigga nigga niggaNigga nigga nigga Nigga nigga nigga Nigga nigga
+niggavNigga nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga
+nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga nigga
+niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga nigga niggaNigga
+nigga nigga Nigga nigga niggaNigga nigga niggaNigga nigga niggaNigga nigga nigga
+Nigga nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga nigga
+niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga nigga niggaNigga
+nigga niggaNigga nigga nigga Nigga nigga niggaNigga nigga niggaNigga nigga
+niggaNigga nigga nigga Nigga nigga niggaNigga nigga niggaNigga nigga niggaNigga
+nigga nigga Nigga nigga niggaNigga nigga niggaNigga nigga niggaNigga nigga nigga
+Nigga nigga niggaNigga nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga
+niggaNigga nigga niggaNigga nigga niggaNigga nigga nigga Nigga nigga niggaNigga
+nigga niggaNigga nigga niggaNigga nigga niggaNigga nigga nigga
