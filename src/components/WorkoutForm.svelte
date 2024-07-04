f6f3bc2a23b49f7646a88e9354018391b36707ad
fischern0 <s6nsfisc@uni-bonn.de>
2024-07-04 18:33:09 +0200

    use daisyUI and improve styling

--- src/components/WorkoutForm.svelte
+++ src/components/WorkoutForm.svelte
@@ -7,7 +7,7 @@
 
   let frequency = 4;
   let duration = 60;
-  let goal = "";
+  let level = "anfänger";
   let subGoal = "";
 
   onMount(() => {
@@ -37,143 +37,93 @@
 
   function handleSubmit(event: Event) {
     event.preventDefault();
-    const result = createSplit(exercises, frequency, duration, "Experte");
+    const result = createSplit(exercises, frequency, duration, level);
     console.log(result);
   }
 </script>
 
-<main
-  class="container mx-auto rounded-lg bg-gray-100 p-4 text-gray-900 shadow-lg"
->
-  <h1 class="mb-6 text-3xl font-bold text-blue-700">Workout Planner</h1>
-
-  <form
-    on:submit={handleSubmit}
-    class="space-y-8 rounded-lg bg-white p-6 shadow-md"
-  >
-    <!-- Training Frequency -->
-    <div class="space-y-2">
-      <label class="text-xl font-bold text-blue-600" for="frequency">
+<main class="container m-auto">
+  <form on:submit={handleSubmit}>
+    <h1 class="mb-6 text-left text-3xl font-bold text-neutral">
+      Workout Planner
+    </h1>
+    <div class="m-2 space-y-2">
+      <label class="text-xl font-bold text-neutral" for="frequency">
         1. Wie oft hast du vor zu trainieren?
       </label>
       <input
         type="range"
-        id="frequency"
-        name="frequency"
+        class="range range-error"
         min="1"
         max="7"
         bind:value={frequency}
         on:input={handleFrequencyChange}
-        class="mt-2 w-full"
       />
-      <div class="mt-2 text-center">
-        <span class="text-2xl text-gray-600">{frequency}x</span>
-        <span class="text-gray-700">Woche</span>
+      <div class="mt-3 text-center">
+        <span class="text-2xl text-neutral">{frequency}x</span>
+        <span class="text-neutral">Woche</span>
       </div>
-      <div id="restDayHint" class="mt-2 hidden text-center text-red-500">
+      <div id="restDayHint" class="mt-2 hidden text-center text-error">
         Es wird empfohlen, mindestens 1 Ruhetag pro Woche zu haben.
       </div>
     </div>
 
-    <!-- Training Duration -->
-    <div class="space-y-2">
-      <label class="text-xl font-bold text-blue-600" for="duration">
+    <div class="m-2 space-y-2">
+      <label class="text-xl font-bold text-neutral" for="duration">
         2. Wie lange hast du vor zu trainieren?
       </label>
       <input
         type="range"
-        id="duration"
-        name="duration"
+        class="range range-error"
         min="30"
         max="180"
         step="10"
         bind:value={duration}
         on:input={handleDurationChange}
-        class="mt-2 w-full"
       />
-      <div class="mt-2 text-center">
-        <span class="text-2xl text-gray-600">{duration} min</span>
+      <div class="mt-3 text-center">
+        <span class="text-2xl text-neutral">{duration} min</span>
       </div>
     </div>
 
-    <!-- Goal -->
-    <div class="space-y-2">
-      <label class="text-xl font-bold text-blue-600"
-        >3. Was ist dein Ziel?</label
-      >
-      <div class="mt-2 flex space-x-4">
-        <label class="flex items-center">
-          <input
-            type="radio"
-            name="goal"
-            value="abnehmen"
-            bind:group={goal}
-            class="mr-2"
-          />
-          <span class="text-gray-700">Abnehmen</span>
-        </label>
-        <label class="flex items-center">
-          <input
-            type="radio"
-            name="goal"
-            value="zunehmen"
-            bind:group={goal}
-            class="mr-2"
-          />
-          <span class="text-gray-700">Zunehmen</span>
-        </label>
-        <label class="flex items-center">
-          <input
-            type="radio"
-            name="goal"
-            value="fitbleiben"
-            bind:group={goal}
-            class="mr-2"
-          />
-          <span class="text-gray-700">Fit bleiben</span>
-        </label>
-      </div>
-    </div>
-    <div class="space-y-2">
-      <label class="text-xl font-bold text-blue-600"
-        >4. Was ist dein Ziel?</label
-      >
-      <div class="mt-2 flex space-x-4">
-        <label class="flex items-center">
-          <input
-            type="radio"
-            name="subGoal"
-            value="kraft"
-            bind:group={subGoal}
-            class="mr-2"
-          />
-          <span class="text-gray-700">Kraft</span>
-        </label>
-        <label class="flex items-center">
-          <input
-            type="radio"
-            name="subGoal"
-            value="muskel"
-            bind:group={subGoal}
-            class="mr-2"
-          />
-          <span class="text-gray-700">Muskel</span>
-        </label>
-        <label class="flex items-center">
-          <input
-            type="radio"
-            name="subGoal"
-            value="ausdauer"
-            bind:group={subGoal}
-            class="mr-2"
-          />
-          <span class="text-gray-700">Ausdauer</span>
-        </label>
-      </div>
-    </div>
+    <div class="m-2">
+      <label class="text-xl font-bold text-neutral" for="level">
+        3. Wie viel Erfahrung hast du?
+      </label>
+      <div class="mt-3 flex space-x-4">
+        <input
+          type="radio"
+          id="beginner"
+          name="level"
+          value="anfänger"
+          class="radio"
+          checked={true}
+          bind:group={level}
+        />
+        <label for="beginner">Anfänger</label>
 
-    <!-- Submit Button -->
-    <div class="text-center">
+        <input
+          type="radio"
+          id="intermediate"
+          name="level"
+          value="fortgeschritten"
+          class="radio"
+          bind:group={level}
+        />
+        <label for="intermediate">Fortgeschritten</label>
+
+        <input
+          type="radio"
+          id="expert"
+          name="level"
+          value="experte"
+          class="radio"
+          bind:group={level}
+        />
+        <label for="expert">Experte</label>
+      </div>
+    </div>
+    <div class="mt-10 text-center">
       <button
         type="submit"
         class="rounded bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
