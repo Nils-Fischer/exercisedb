a468e68e19e42aa9ab448c1d0900b8375870d039
fischern0 <s6nsfisc@uni-bonn.de>
2024-07-04 15:58:43 +0200

    adjust default styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,12 +15,12 @@ export default {
       {
         emerald: {
           ...require("daisyui/src/theming/themes")["emerald"],
-          "--rounded-box": "0.2rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-box": "0.7rem", // border radius rounded-box utility class, used in card and other large boxes
           "--rounded-btn": "0.1rem", // border radius rounded-btn utility class, used in buttons and similar element
           "--rounded-badge": "0.6rem", // border radius rounded-badge utility class, used in badges and similar
-          "--animation-btn": "0.25s", // duration of animation when you click on button
-          "--animation-input": "0.7s", // duration of animation for inputs like checkbox, toggle, radio, etc
-          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--animation-btn": "0.01s", // duration of animation when you click on button
+          "--animation-input": "0.1s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.98", // scale transform of button when you focus on it
           "--border-btn": "2px", // border width of buttons
           "--tab-border": "1px", // border width of tabs
           "--tab-radius": "0.1rem", // border radius of tabs
