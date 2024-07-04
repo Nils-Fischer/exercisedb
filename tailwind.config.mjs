5edafac0180d8264a4153a07d7556ffeed6889e4
fischern0 <s6nsfisc@uni-bonn.de>
2024-07-04 15:11:38 +0200

    customize emerald theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,7 +11,25 @@ export default {
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
   daisyui: {
-    themes: ["light", "dark", "emerald"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: [
+      {
+        emerald: {
+          ...require("daisyui/src/theming/themes")["emerald"],
+          "--rounded-box": "0.2rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.1rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "0.6rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.7s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "2px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.1rem", // border radius of tabs
+        },
+      },
+      "light",
+      "dark",
+      "corporate",
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
