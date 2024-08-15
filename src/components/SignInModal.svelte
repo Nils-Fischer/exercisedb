f35a02eeddf4f8c94deec018ad39efe1a82f4940
fischern0 <s6nsfisc@uni-bonn.de>
2024-08-15 14:45:33 +0200

    use a form element for sign up

--- src/components/SignInModal.svelte
+++ src/components/SignInModal.svelte
@@ -22,7 +22,8 @@
     isEmailValid = validateEmail(email);
   }
 
-  async function handleSignIn() {
+  async function handleSubmit(event: Event) {
+    event.preventDefault();
     if (!isEmailValid) {
       return;
     }
@@ -89,55 +90,57 @@
     ></button>
     <div class="z-[101] flex w-96 flex-col gap-4 rounded-box bg-base-100 p-6" transition:fade={{ duration: 100 }}>
       <h1 class="self-center text-3xl font-bold">Anmelden</h1>
-      <label class="form-control">
-        <div class="label">
-          <span class="label-text">E-Mail</span>
-        </div>
-        <input
-          type="email"
-          bind:value={email}
-          on:input={handleEmailInput}
-          class="input input-bordered w-full {!isEmailValid && email ? 'input-error' : ''}"
-          placeholder="Geben Sie Ihre E-Mail ein"
-          required
-        />
-        {#if !isEmailValid && email}
-          <span class="label-text-alt mt-1 text-error">Bitte geben Sie eine gültige E-Mail-Adresse ein.</span>
-        {/if}
-      </label>
-      <label class="form-control">
-        <div class="label">
-          <span class="label-text">Passwort</span>
-          <button on:click={handleForgotPassword} class="link label-text link-accent">Passwort vergessen?</button>
-        </div>
-        <input
-          type="password"
-          bind:value={password}
-          class="input input-bordered w-full"
-          placeholder="Geben Sie Ihr Passwort ein"
-          required
-        />
-      </label>
-      <div class="form-control">
-        <label class="label cursor-pointer gap-2 self-start">
-          <input type="checkbox" class="checkbox" bind:checked={rememberMe} />
-          <span class="label-text">Angemeldet bleiben</span>
+      <form on:submit={handleSubmit} class="flex flex-col gap-4">
+        <label class="form-control">
+          <div class="label">
+            <span class="label-text">E-Mail</span>
+          </div>
+          <input
+            type="email"
+            bind:value={email}
+            on:input={handleEmailInput}
+            class="input input-bordered w-full {!isEmailValid && email ? 'input-error' : ''}"
+            placeholder="Geben Sie Ihre E-Mail ein"
+            required
+          />
+          {#if !isEmailValid && email}
+            <span class="label-text-alt mt-1 text-error">Bitte geben Sie eine gültige E-Mail-Adresse ein.</span>
+          {/if}
         </label>
-      </div>
-      {#if errorMessage}
-        <span class="text-sm text-error">{errorMessage}</span>
-      {/if}
-      <button class="btn btn-primary w-full" on:click={handleSignIn} disabled={!isEmailValid || email === ""}
-        >Anmelden</button
-      >
+        <label class="form-control">
+          <div class="label">
+            <span class="label-text">Passwort</span>
+            <button type="button" on:click={handleForgotPassword} class="link label-text link-accent"
+              >Passwort vergessen?</button
+            >
+          </div>
+          <input
+            type="password"
+            bind:value={password}
+            class="input input-bordered w-full"
+            placeholder="Geben Sie Ihr Passwort ein"
+            required
+          />
+        </label>
+        <div class="form-control">
+          <label class="label cursor-pointer gap-2 self-start">
+            <input type="checkbox" class="checkbox" bind:checked={rememberMe} />
+            <span class="label-text">Angemeldet bleiben</span>
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary w-full" disabled={!isEmailValid}>Anmelden</button>
+        {#if errorMessage}
+          <span class="text-sm text-error">{errorMessage}</span>
+        {/if}
+      </form>
       <div class="divider">ODER</div>
-      <button class="btn btn-outline w-full" on:click={handleGoogleSignIn}>
+      <button type="button" class="btn btn-outline w-full" on:click={handleGoogleSignIn}>
         <i class="fa-brands fa-google mr-2"></i>
         Mit Google anmelden
       </button>
       <div class="mt-4 text-center">
         <span>Noch kein Konto?</span>
-        <button on:click={switchToRegister} class="link link-secondary ml-1">Registrieren</button>
+        <button type="button" on:click={switchToRegister} class="link link-secondary ml-1">Registrieren</button>
       </div>
     </div>
   </div>
